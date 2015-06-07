6a93e6fced21a182976b87069a5c8427c0d6e568
Fabricio Cabral <fabriciofx@gmail.com>
2015-06-07 19:41:28 -0300

    Adicionado suite de testes.

---
+++ src/test/java/com/github/fabriciofx/financeiro/dominio/Testes.java
@@ -0,0 +1,10 @@
+package com.github.fabriciofx.financeiro.dominio;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ TesteRegra.class, TesteConta.class, })
+public class Testes {
+
+}
