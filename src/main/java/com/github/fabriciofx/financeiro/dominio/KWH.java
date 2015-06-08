81ecdc7819a72e50034e57b0b091900be093d7fe
Fabricio Cabral <fabriciofx@gmail.com>
2015-06-08 11:19:20 -0300

    Adicionado classe que representa KWH.

---
+++ src/main/java/com/github/fabriciofx/financeiro/dominio/KWH.java
@@ -0,0 +1,13 @@
+package com.github.fabriciofx.financeiro.dominio;
+
+public final class KWH {
+	private final int valor;
+
+	public KWH(final int valor) {
+		this.valor = valor;
+	}
+
+	public int valor() {
+		return valor;
+	}
+}
