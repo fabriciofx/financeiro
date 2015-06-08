adc78fc3f77d5f4cdf664c23ca089d4a4d14db58
Fabricio Cabral <fabriciofx@gmail.com>
2015-06-08 00:13:08 -0300

    Incluido o teste de Dinheiro na suite de testes.

--- src/test/java/com/github/fabriciofx/financeiro/dominio/Testes.java
+++ src/test/java/com/github/fabriciofx/financeiro/dominio/Testes.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ TesteRegra.class, TesteConta.class, })
+@Suite.SuiteClasses({ TesteRegra.class, TesteConta.class, TesteDinheiro.class })
 public class Testes {
 
 }
